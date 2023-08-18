3b445646709f015428c2ad34d3ef883c3ec045b1
Sumanth <sumanthale@gmail.com>
2023-08-18 13:45:34 +0530

    fitness changes2

--- src/views/Fitness.jsx
+++ src/views/Fitness.jsx
@@ -14,17 +14,53 @@ const Fitness = () => {
       <MiniHeader heading={"Fitness"} subHeading={""} />
       <div className="grid grid-cols-1  md:grid-cols-12 gap-6 gap-x-10  pt-6  lg:pr-10 pb-6">
         <div className="md:col-span-4 md:order-last mb-2 pr-4  pl-4 ">
-          <p className="block md:hidden text-justify mb-6">
-            Discover the ultimate full-service fitness center that's all about
-            you! Get ready to achieve your fitness dreams with top-notch gym
-            equipment, fun exercise classes, and personalized training plans.
-            And guess what? We're more than just a gym! Treat yourself to
-            relaxing spa services, learn about healthy eating with expert
-            advice, and find your zen in our calming relaxation areas. Join a
-            friendly community of fitness pros and friends who will cheer you on
-            every step of the way. Your wellness adventure starts here - come on
-            over and let's make healthy happen, together!
-          </p>
+          <div className="block md:hidden pl-4 lg:pl-14">
+            <p className="text-justify ">
+              Discover the ultimate full-service fitness center that's all about
+              you! Get ready to achieve your fitness dreams with top-notch gym
+              equipment, fun exercise classes, and personalized training plans.
+              And guess what? We're more than just a gym! Treat yourself to
+              relaxing spa services, learn about healthy eating with expert
+              advice, and find your zen in our calming relaxation areas. Join a
+              friendly community of fitness pros and friends who will cheer you
+              on every step of the way. Your wellness adventure starts here -
+              come on over and let's make healthy happen, together!
+            </p>
+            <div className="py-4 pr-4 lg:pr-0 ">
+              <Carousel
+                autoPlay
+                infiniteLoop
+                swipeable={false}
+                showArrows={false}
+                showStatus={false}
+                showThumbs={false}
+                showIndicators={false}
+                interval={10000}
+                stopOnHover={false}
+                className="carousel"
+                style={{ maxHeight: "100%", maxWidth: "60%", margin: "0 auto" }}
+              >
+                <div
+                  className="w-full carousel-image-item h-96 bg-cover bg-center"
+                  style={{
+                    backgroundImage: `url(${Fitness1})`,
+                  }}
+                ></div>
+                <div
+                  className="w-full carousel-image-item h-96 bg-cover bg-center"
+                  style={{
+                    backgroundImage: `url(${Fitness2})`,
+                  }}
+                ></div>
+                <div
+                  className="w-full carousel-image-item h-96 bg-cover bg-center"
+                  style={{
+                    backgroundImage: `url(${Fitness3})`,
+                  }}
+                ></div>
+              </Carousel>
+            </div>
+          </div>
           <div className="border-4  gap-4 p-5 border-[#7da33f]">
             <h1 className="text-[#7da33f] font-bold text-2xl text-center">
               PERSONAL TRAINING
@@ -64,8 +100,8 @@ const Fitness = () => {
         </div>
 
         <div className="md:col-span-8">
-          <div className="pl-4 lg:pl-14">
-            <p className="hidden md:block text-justify ">
+          <div className="hidden md:block pl-4 lg:pl-14">
+            <p className="text-justify ">
               Discover the ultimate full-service fitness center that's all about
               you! Get ready to achieve your fitness dreams with top-notch gym
               equipment, fun exercise classes, and personalized training plans.
