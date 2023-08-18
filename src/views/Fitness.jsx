903fb2468aacd01663762e91d12afad016d9bbac
Sumanth <sumanthale@gmail.com>
2023-08-18 13:41:40 +0530

    fitnesss

--- src/views/Fitness.jsx
+++ src/views/Fitness.jsx
@@ -1,11 +1,6 @@
 import MiniHeader from "../layout/MainLayout/MiniHeader";
 import Footer from "../reusable/Footer";
-import FtinessImg from "../assets/golf/fitness-header.png";
-import FitnessCards from "../components/Reusable/FitnessCards";
-import LeaveAMessage from "../components/Home/LeaveAMessage";
 import { Link } from "react-router-dom";
-import GolfBallPattern from "../assets/golf/golfballpattern.png";
-import MapCourse from "../assets/golf/mapcourse.png";
 import { Carousel } from "react-responsive-carousel";
 import Fitness1 from "../assets/golf/fitness1.png";
 import Fitness2 from "../assets/golf/fitness2.png";
@@ -17,66 +12,71 @@ const Fitness = () => {
   return (
     <>
       <MiniHeader heading={"Fitness"} subHeading={""} />
-      <div className="grid grid-cols-1  md:grid-cols-12 gap-6  pt-6 px-8">
-        <div className="md:col-span-4 md:order-last mb-2">
+      <div className="grid grid-cols-1  md:grid-cols-12 gap-6 gap-x-10  pt-6  lg:pr-10 pb-6">
+        <div className="md:col-span-4 md:order-last mb-2 pr-4  pl-4 ">
           <p className="block md:hidden text-justify mb-6">
-          Discover the ultimate full-service fitness center that's all about you! Get ready to achieve your fitness dreams with top-notch gym equipment, fun exercise classes, and personalized training plans. 
-          And guess what? We're more than just a gym! Treat yourself to relaxing spa services, learn about healthy eating with expert advice, and find your zen in our calming relaxation areas.
-           Join a friendly community of fitness pros and friends who will cheer you on every step of the way. 
-           Your wellness adventure starts here - come on over and let's make healthy happen, together!
+            Discover the ultimate full-service fitness center that's all about
+            you! Get ready to achieve your fitness dreams with top-notch gym
+            equipment, fun exercise classes, and personalized training plans.
+            And guess what? We're more than just a gym! Treat yourself to
+            relaxing spa services, learn about healthy eating with expert
+            advice, and find your zen in our calming relaxation areas. Join a
+            friendly community of fitness pros and friends who will cheer you on
+            every step of the way. Your wellness adventure starts here - come on
+            over and let's make healthy happen, together!
           </p>
-          <div className="border-2 gap-4 p-4 border-[#7da33f]" style={{}}>
+          <div className="border-4  gap-4 p-5 border-[#7da33f]">
             <h1 className="text-[#7da33f] font-bold text-2xl text-center">
               PERSONAL TRAINING
             </h1>
             <div className="px-2">
-              <p className="text-left  my-3">
-              Schedule an opportunity to talk to our physical trainer. Jake is a trainer, coach, group fitness instructor and massage therapist of 20+ years. 
-              A veteran of the US Navy, he began his fitness journey helping fellow crew members on a ballistic missile submarine while underway during patrols. 
-              After his time in the service, it was a seamless transition to becoming a fitness professional and health advocate. 
-              His perspective about health and our body is that it is a gift and should not be taken for granted. 
-              He believes balance, functionality, mobility, and strength are paramount to any fitness regimen.
-              So whatever your health and fitness goals, let Jake help you to create and execute a plan to achieve them
+              <p className="text-left  my-4">
+                Schedule an opportunity to talk to our physical trainer. Jake is
+                a trainer, coach, group fitness instructor and massage therapist
+                of 20+ years. A veteran of the US Navy, he began his fitness
+                journey helping fellow crew members on a ballistic missile
+                submarine while underway during patrols. After his time in the
+                service, it was a seamless transition to becoming a fitness
+                professional and health advocate. His perspective about health
+                and our body is that it is a gift and should not be taken for
+                granted. He believes balance, functionality, mobility, and
+                strength are paramount to any fitness regimen. So whatever your
+                health and fitness goals, let Jake help you to create and
+                execute a plan to achieve them
               </p>
 
-           
+              <img
+                src={Training}
+                alt="Golf Course"
+                className="object-fill my-4"
+                style={{ width: "100%", height: "auto", maxHeight: "350px" }}
+              />
 
-          <img
-            src={Training}
-            alt="Golf Course"
-            className="object-fill"
-            style={{ width: "100%", height: "auto", maxHeight: "350px" }}
-          />
-
-
-
-          
-              <button className="uppercase outline outline-1 px-2 text-[#7da33f] w-full text-xl my-3 tracking-wider outline-[#7da33f]">
-  <Link to="/tee-times" className="block w-full h-full">
-  CONTACT US
-  </Link>
-</button>
+              <button className="uppercase outline outline-2 px-2 text-[#7da33f] w-full text-xl my-3 mb-0 tracking-wider  outline-[#7da33f] font-semibold">
+                <Link to="/tee-times" className="block w-full h-full">
+                  CONTACT US
+                </Link>
+              </button>
             </div>
 
-           
-            <div className="px-2">
-            
-            </div>
+            <div className="px-2"></div>
           </div>
         </div>
 
         <div className="md:col-span-8">
-          <p className="hidden md:block text-justify mb-6">
-          Discover the ultimate full-service fitness center that's all about you! 
-          Get ready to achieve your fitness dreams with top-notch gym equipment, fun exercise classes, and personalized training plans. 
-          And guess what? We're more than just a gym! Treat yourself to relaxing spa services, learn about healthy eating with expert advice, and find your zen in our calming relaxation areas. 
-          Join a friendly community of fitness pros and friends who will cheer you on every step of the way. 
-          Your wellness adventure starts here - come on over and let's make healthy happen, together!
-          </p>
-        
-
-
-<div className="md:col-span-3 py-4">
+          <div className="pl-4 lg:pl-14">
+            <p className="hidden md:block text-justify ">
+              Discover the ultimate full-service fitness center that's all about
+              you! Get ready to achieve your fitness dreams with top-notch gym
+              equipment, fun exercise classes, and personalized training plans.
+              And guess what? We're more than just a gym! Treat yourself to
+              relaxing spa services, learn about healthy eating with expert
+              advice, and find your zen in our calming relaxation areas. Join a
+              friendly community of fitness pros and friends who will cheer you
+              on every step of the way. Your wellness adventure starts here -
+              come on over and let's make healthy happen, together!
+            </p>
+            <div className="py-4 pr-4 lg:pr-0 ">
               <Carousel
                 autoPlay
                 infiniteLoop
@@ -91,57 +91,42 @@ const Fitness = () => {
                 style={{ maxHeight: "100%", maxWidth: "60%", margin: "0 auto" }}
               >
                 <div
-                  className="w-full carousel-image-item h-56 bg-cover bg-center"
+                  className="w-full carousel-image-item h-96 bg-cover bg-center"
                   style={{
                     backgroundImage: `url(${Fitness1})`,
                   }}
                 ></div>
                 <div
-                  className="w-full carousel-image-item h-56 bg-cover bg-center"
+                  className="w-full carousel-image-item h-96 bg-cover bg-center"
                   style={{
                     backgroundImage: `url(${Fitness2})`,
                   }}
                 ></div>
                 <div
-                  className="w-full carousel-image-item h-56 bg-cover bg-center"
+                  className="w-full carousel-image-item h-96 bg-cover bg-center"
                   style={{
                     backgroundImage: `url(${Fitness3})`,
                   }}
                 ></div>
-               
               </Carousel>
-            </div>
-
-
-  
-           
-
-            <div
-            className="bg-red-800 p-6 text-white relative"
-            style={{ marginTop: "20px" }}
-          >
-            <div className="col-span-1">
-              <img src={Yoga} alt="Etiquette" className="max-w-28 h-auto" />
-            </div>
-            <div className="col-span-1 pt-4 md:pl-4 text-sm list-disc">
-            <h1 className="text-3xl font-bold">KNOW THE ETIQUETTE</h1>
-            <ul className="pt-4 pl-4 text-sm list-disc">
-              <li>Show up early to take advantage of your tee time</li>
-              <li>Obey the cart rules & stay on the path</li>
-              <li>Warm up with three balls</li>
-              <li>Don't talk & stand away</li>
-              <li>Only look for a lost ball for three minutes</li>
-              <li>Be respectful of time and play at an appropriate pace</li>
-              <li>Be prepared to play when it's your time to go</li>
-              <li>Stay off your phone as much as you can</li>
-              <li>Yell FORE when a ball is going toward another person</li>
-              <li>Fix your ball mark after you hit when on the green</li>
-            </ul>
             </div>
           </div>
 
-           
-          
+          <div
+            className="bg-red-800 p-6 text-white relative grid-cols-1 grid lg:grid-cols-2 justify-items-center"
+            style={{ marginTop: "20px" }}
+          >
+            <img src={Yoga} alt="Etiquette" className="max-w-xs " />
+            <div className=" pt-4 md:pl-4">
+              <h1 className="text-3xl font-bold mb-3">GET YOUR ZEN ON</h1>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Temporibus, consequatur amet quam ad tenetur ipsam earum. Quae
+              officiis maiores quis alias numquam molestiae. Facilis sapiente,
+              dolorum natus voluptatem dolores a? Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Temporibus, consequatur amet quam ad
+              tenetur ipsam earum. Quae officiis maiores quis alias numquam
+            </div>
+          </div>
         </div>
       </div>
 
@@ -152,6 +137,3 @@ const Fitness = () => {
 };
 
 export default Fitness;
-
-
-
--- src/views/Golf.jsx
+++ src/views/Golf.jsx
@@ -15,16 +15,13 @@ import { Link } from "react-router-dom";
 import "../index.css";
 import { Carousel } from "react-responsive-carousel";
 import GolfBallPattern from "../assets/golf/golfballpattern.png";
-import Tsheet from "./Tsheet";
-import Memberships from "../components/Golf/Memberships";
-
 
 const Golf = () => {
   return (
     <>
       <MiniHeader heading={"Golf"} subHeading={""} />
-      <div className="grid grid-cols-1  md:grid-cols-12 gap-6  pt-6 px-8">
-        <div className="md:col-span-4 md:order-last mb-2">
+      <div className="grid grid-cols-1  md:grid-cols-12 gap-6 gap-x-12  pt-6 w-full lg:max-w-[90vw] mx-auto px-4">
+        <div className="md:col-span-4 md:order-last mb-2 ">
           <p className="block md:hidden text-justify mb-6">
             Nestled amidst rolling hills and picturesque landscapes, the quaint
             9-hole golf course offers a delightful escape for both novice and
@@ -40,15 +37,17 @@ const Golf = () => {
             Texas 9 are sure to relish in its tranquil charm and natural beauty.
           </p>
           <div className="border-2 gap-4 p-4 border-[#7da33f]" style={{}}>
-            <h1 className="text-[#7da33f] font-bold text-2xl text-center">
+            <h1 className="text-[#7da33f] font-bold text-2xl text-center my-4">
               9 HOLE EXECUTIVE COURSE
             </h1>
-            <div className="px-2">
-              <p className="text-left  my-3">
-              Enjoy a quick game of golf on-foot, or upgrade your golfing adventure to include a cart full of modern comforts to get you around.
+            <div className="px-2 text">
+              <p className="text-left  my-4">
+                Enjoy a quick game of golf on-foot, or upgrade your golfing
+                adventure to include a cart full of modern comforts to get you
+                around.
               </p>
 
-              <ul className="list-disc list-inside ">
+              <ul className="list-disc list-inside pl-4 lg:pl-10">
                 <li className="text-xl uppercase font-semibold">
                   Monday - Thursday
                   <br />
@@ -63,14 +62,14 @@ const Golf = () => {
                   </ul>
                 </li>
               </ul>
-              <ul className="list-disc list-inside mt-4">
+              <ul className="list-disc list-inside mt-4 pl-4 lg:pl-10">
                 <li className="text-xl uppercase font-semibold">
                   Friday - Sunday
                   <br />
                   <span className="ml-7">7 Am - Sunset</span>
                   <ul className="list-disc list-inside pl-4">
                     <li className="lowercase font-thin text-lg">
-                       course - $10/<span className="text-sm">person</span> 
+                      course - $10/<span className="text-sm">person</span>
                     </li>
                     <li className="lowercase font-thin text-lg">
                       +golf cart - $18/<span className="text-sm">person</span>
@@ -79,47 +78,49 @@ const Golf = () => {
                 </li>
               </ul>
 
-          
-              <button className="uppercase outline outline-1 px-2 text-[#7da33f] w-full text-xl my-3 tracking-wider outline-[#7da33f]">
-  <Link to="/tee-times" className="block w-full h-full">
-  Book Your Tee time
-  </Link>
-</button>
+              <button className="uppercase outline outline-1 px-2 text-[#7da33f] w-full text-xl my-6 tracking-wider outline-[#7da33f]">
+                <Link to="/tee-times" className="block w-full h-full">
+                  Book Your Tee time
+                </Link>
+              </button>
             </div>
 
-            <h1 className="text-[#7da33f] font-bold text-2xl  uppercase my-3 text-left">
+            <h1 className="text-[#7da33f] font-bold text-2xl  uppercase mt-6 my-4 text-left">
               Driving range
             </h1>
             <div className="px-2">
-              <p className="text-left my-3 ">
-              Choose your bucket to perfect vour swing anvtime of day even after sunset with our lighted driving range!
+              <p className="text-left my-4 ">
+                Choose your bucket to perfect vour swing anvtime of day even
+                after sunset with our lighted driving range!
               </p>
 
-              <ul className="list-disc list-inside ">
+              <ul className="list-disc list-inside pl-4 lg:pl-10 my-4">
                 <li className="text-xl uppercase font-Gboldoblique font-semibold">
                   Monday - Sunday
                   <br />
                   <span className="ml-7">7 Am - 9 Pm</span>
-                 
                   <ul className="list-disc list-inside pl-4">
                     <li className="lowercase font-thin text-lg">
-                     small bucket - $10{/*<span className="text-sm">person</span> */}
+                      small bucket - $10
+                      {/*<span className="text-sm">person</span> */}
                     </li>
                     <li className="lowercase font-thin text-lg">
-                    medium bucket - $12{/*<span className="text-sm">person</span> */}
+                      medium bucket - $12
+                      {/*<span className="text-sm">person</span> */}
                     </li>
                     <li className="lowercase font-thin text-lg">
-                    large bucket - $15{/*<span className="text-sm">person</span> */}
+                      large bucket - $15
+                      {/*<span className="text-sm">person</span> */}
                     </li>
                   </ul>
                 </li>
               </ul>
 
-              <button className="uppercase outline outline-1 px-2 text-[#7da33f] w-full text-xl my-3 tracking-wider outline-[#7da33f]">
-  <Link to="/golf/memberships" className="block w-full h-full">
-    Play more save more with a membership!
-  </Link>
-</button>
+              <button className="uppercase outline outline-1 px-2 text-[#7da33f] w-full text-xl my-4 mb-6 tracking-wider outline-[#7da33f]">
+                <Link to="/golf/memberships" className="block w-full h-full">
+                  Play more save more with a membership!
+                </Link>
+              </button>
             </div>
           </div>
         </div>
@@ -146,7 +147,7 @@ const Golf = () => {
             style={{ width: "100%", height: "auto", maxHeight: "350px" }}
           />
           <div
-            className="bg-pattern grid grid-col-1  md:grid-cols-10 relative bg-contain md:bg-cover"
+            className="bg-pattern grid grid-col-1  md:grid-cols-10 relative bg-contain md:bg-cover opacity-80"
             style={{
               backgroundImage: `url(${GolfBallPattern})`,
               marginTop: "10px",
@@ -156,19 +157,23 @@ const Golf = () => {
             <h1 className="absolute text-xl md:text-3xl font-bold left-10">
               KNOW THE ETIQUTTE
             </h1>
-            <div className="md:col-start-4 md:col-span-4 pl-1 pt-10 text-sm">
-
-              <li>show up early to take advantage of your tee time </li>
+            <ul className="list-disc md:col-start-4 md:col-span-4 pt-10 mx-auto px-6">
+              <li>
+                show up early to take advantage of <br />
+                your tee time{" "}
+              </li>
               <li>obey the cart rules & stay on the path </li>
               <li>warm up with three balls</li>
               <li>don't talk & stand away </li>
               <li>only look tor a lost ball for three min </li>
               <li>be respectful of time and play at an appropriate pace </li>
-              <li>be prepared to play when its your time to go </li>
+              <li>
+                be prepared to play when its your <br /> time to go{" "}
+              </li>
               <li>stay off your phone as much as you can</li>
               <li>yell FORE when a ball is going toward another person </li>
               <li>fix your ball mark after you hit when on the green </li>
-            </div>
+            </ul>
 
             <div className="md:col-span-3 py-4">
               <Carousel
@@ -185,37 +190,37 @@ const Golf = () => {
                 style={{ maxHeight: "100%", maxWidth: "60%", margin: "0 auto" }}
               >
                 <div
-                  className="w-full carousel-image-item h-56 bg-cover bg-center"
+                  className="w-full carousel-image-item h-[350px] bg-cover bg-center"
                   style={{
                     backgroundImage: `url(${Golf1})`,
                   }}
                 ></div>
                 <div
-                  className="w-full carousel-image-item h-56 bg-cover bg-center"
+                  className="w-full carousel-image-item h-[350px] bg-cover bg-center"
                   style={{
                     backgroundImage: `url(${Golf2})`,
                   }}
                 ></div>
                 <div
-                  className="w-full carousel-image-item h-56 bg-cover bg-center"
+                  className="w-full carousel-image-item h-[350px] bg-cover bg-center"
                   style={{
                     backgroundImage: `url(${Golf3})`,
                   }}
                 ></div>
                 <div
-                  className="w-full carousel-image-item h-56 bg-cover bg-center"
+                  className="w-full carousel-image-item h-[350px] bg-cover bg-center"
                   style={{
                     backgroundImage: `url(${Golf4})`,
                   }}
                 ></div>
                 <div
-                  className="w-full carousel-image-item h-56 bg-cover bg-center"
+                  className="w-full carousel-image-item h-[350px] bg-cover bg-center"
                   style={{
                     backgroundImage: `url(${Golf5})`,
                   }}
                 ></div>
                 <div
-                  className="w-full carousel-image-item h-56 bg-cover bg-center"
+                  className="w-full carousel-image-item h-[350px] bg-cover bg-center"
                   style={{
                     backgroundImage: `url(${Golf6})`,
                   }}
