b1db580f610899fb615fd90eb218093239d9178f
JavvajiSumanth <sumanthjavvaji1299@gmail.com>
2023-08-24 02:55:03 -0500

    new-golf

--- src/views/Golf.jsx
+++ src/views/Golf.jsx
@@ -21,8 +21,8 @@ const Golf = () => {
     <>
       <MiniHeader heading={"Golf"} subHeading={""} />
       <div className="grid grid-cols-1  md:grid-cols-12 gap-6 gap-x-12  pt-6 w-full lg:max-w-[90vw] mx-auto px-4">
-        <div className="md:col-span-4 md:order-last mb-2 ">
-          <p className="block md:hidden text-justify mb-6">
+      <div className="md:col-span-4 md:order-last mb-2 ">
+          <div className="block md:hidden text-justify mb-6">
             Nestled amidst rolling hills and picturesque landscapes, the quaint
             9-hole golf course offers a delightful escape for both novice and
             experienced golfers alike. Its compact layout weaves through lush
@@ -35,97 +35,37 @@ const Golf = () => {
             passion for the game in a serene environment. Whether seeking a
             casual round or an opportunity to refine their skills, visitors to
             Texas 9 are sure to relish in its tranquil charm and natural beauty.
-          </p>
-          <div className="border-2 gap-4 p-4 border-[#7da33f]" style={{}}>
-            <h1 className="text-[#7da33f] font-bold text-2xl text-center my-4">
-              9 HOLE EXECUTIVE COURSE
-            </h1>
-            <div className="px-2 text">
-              <p className="text-left  my-4">
-                Enjoy a quick game of golf on-foot, or upgrade your golfing
-                adventure to include a cart full of modern comforts to get you
-                around.
-              </p>
-
-              <ul className="list-disc list-inside pl-4 lg:pl-10">
-                <li className="text-xl uppercase font-semibold">
-                  Monday - Thursday
-                  <br />
-                  <span className="ml-7">7 Am - Sunset</span>
-                  <ul className="list-disc list-inside pl-4">
-                    <li className="lowercase font-thin text-lg">
-                      course - $10/<span className="text-sm">person</span>
-                    </li>
-                    <li className="lowercase font-thin text-lg">
-                      +golf cart - $18/<span className="text-sm">person</span>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-              <ul className="list-disc list-inside mt-4 pl-4 lg:pl-10">
-                <li className="text-xl uppercase font-semibold">
-                  Friday - Sunday
-                  <br />
-                  <span className="ml-7">7 Am - Sunset</span>
-                  <ul className="list-disc list-inside pl-4">
-                    <li className="lowercase font-thin text-lg">
-                      course - $10/<span className="text-sm">person</span>
-                    </li>
-                    <li className="lowercase font-thin text-lg">
-                      +golf cart - $18/<span className="text-sm">person</span>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-
-              <button className="uppercase outline outline-1 px-2 text-[#7da33f] w-full text-xl my-6 tracking-wider outline-[#7da33f]">
+          </div>
+          
+          <div className="absolute top-1/2 right-0 transform -translate-y-1/2 pr-6">
+            <div className="mb-8">
+              <button className="uppercase outline outline-1 px-2 text-[#7da33f] text-xl tracking-wider outline-[#7da33f]">
                 <Link to="/tee-times" className="block w-full h-full">
-                  Book Your Tee time
+                  Book Your Tee Time
                 </Link>
               </button>
             </div>
-
-            <h1 className="text-[#7da33f] font-bold text-2xl  uppercase mt-6 my-4 text-left">
-              Driving range
-            </h1>
-            <div className="px-2">
-              <p className="text-left my-4 ">
-                Choose your bucket to perfect vour swing anvtime of day even
-                after sunset with our lighted driving range!
-              </p>
-
-              <ul className="list-disc list-inside pl-4 lg:pl-10 my-4">
-                <li className="text-xl uppercase font-Gboldoblique font-semibold">
-                  Monday - Sunday
-                  <br />
-                  <span className="ml-7">7 Am - 9 Pm</span>
-                  <ul className="list-disc list-inside pl-4">
-                    <li className="lowercase font-thin text-lg">
-                      small bucket - $10
-                      {/*<span className="text-sm">person</span> */}
-                    </li>
-                    <li className="lowercase font-thin text-lg">
-                      medium bucket - $12
-                      {/*<span className="text-sm">person</span> */}
-                    </li>
-                    <li className="lowercase font-thin text-lg">
-                      large bucket - $15
-                      {/*<span className="text-sm">person</span> */}
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-
-              <button className="uppercase outline outline-1 px-2 text-[#7da33f] w-full text-xl my-4 mb-6 tracking-wider outline-[#7da33f]">
-                <Link to="/golf/memberships" className="block w-full h-full">
-                  Play more save more with a membership!
+            <div className="mb-8">
+              <button className="uppercase outline outline-1 px-2 text-maroon text-xl tracking-wider outline-[#791214]">
+                <Link to="/tee-times" className="block w-full h-full">
+                  PLAY MORE
+                  SAVE MORE
+                  WITH A
+                  MEMBERSHIP!
+                </Link>
+              </button>
+            </div>
+            <div>
+              <button className="uppercase outline outline-1 px-2 text-violet text-xl tracking-wider outline-[#172649]">
+                <Link to="/tee-times" className="block w-full h-full">
+                  BOOK A PRIVATE LESSON
                 </Link>
               </button>
             </div>
           </div>
         </div>
 
-        <div className="md:col-span-8">
+         <div className="md:col-span-8">
           <p className="hidden md:block text-justify mb-6">
             Nestled amidst rolling hills and picturesque landscapes, the quaint
             9-hole golf course offers a delightful escape for both novice and
@@ -228,8 +168,8 @@ const Golf = () => {
               </Carousel>
             </div>
           </div>
-        </div>
-      </div>
+        </div> 
+      </div> 
 
       <LeaveAMessage header={"HAVE QUESTIONS?"} />
       <Footer />
