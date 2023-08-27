a5da7f86affb0894c02272f7640d798c708791b0
JavvajiSumanth <sumanthjavvaji1299@gmail.com>
2023-08-27 10:28:10 -0500

    k

--- src/components/Golf/Rates.jsx
+++ src/components/Golf/Rates.jsx
@@ -1,76 +1,145 @@
-import { useEffect } from "react";
+// import React from "react";
 import MiniHeader from "../../layout/MainLayout/MiniHeader";
 import Footer from "../../reusable/Footer";
-import BGImg from "../../assets/golf/GrassWithLake.jpeg";
-import RatesHeader from "../../assets/golf/golf-rates-header.jpg";
-import AllCards from "../Reusable/AllCards";
+import { LiaGolfBallSolid } from "react-icons/lia";
+import LeaveAMessage from "../../components/Home/LeaveAMessage";
+import MapCourse from "../../assets/golf/mapcourse.png";
+import Golfer from "../../assets/golf/rates-golfer.png";
+import DrivingRange from "../../assets/golf/rates-drivingrange.png";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "../../index.css";
+import { Carousel } from "react-responsive-carousel";
+import GolfBallPattern from "../../assets/golf/golfballpattern.png";
 
 const Rates = () => {
   return (
-    <div>
-      <MiniHeader heading={"Rates"} subHeading={""} image={RatesHeader} />
+    <>
+      <MiniHeader heading={"Rates"} subHeading={""} />
 
-      <div className="p-5 md:p-16 bg-[#162649]">
-        <div
-          className="text-slate-100 pb-10 bg-cover bg-center font-DinCondensed"
-          style={{
-            backgroundImage: `url(${BGImg})`,
-          }}
-        >
-          {/* <h1 className="uppercase tracking-wide text-4xl  sm:text-5xl text-black font-bold   font-primary text-center py-8">
-          BOOK YOUR TEE TIME ONLINE
-        </h1>
-        <h1 className="uppercase tracking-wide text-4xl text-black font-bold   font-primary text-center py-4">
-          RATES
-        </h1> */}
-          <div className="grid grid-cols-1 md:grid-cols-2 p-6 justify-items-center gap-y-8 gap-x-10 max-w-6xl mx-auto ">
-            <div className="card">
-              <div className="content">
-                <div className="font-bold text-3xl">MONDAY - THURSDAY</div>
-                <div className="flex justify-around">
-                  <div>
-                    <div className="title">9 HOLES</div>
-                    <div className="price text-green-800">$10</div>
-                  </div>
-                  <div>
-                    <div className="title">9 HOLES + CART </div>
-                    <div className="price text-green-800">$20</div>
-                  </div>
-                </div>
-                <div className="text-md">
-                  Enjoy a quick and affordable golfing experience on-foot, or
-                  upgrade your golfing adventure to include a cart full of
-                  modern comforts.
-                </div>
-              </div>
-            </div>
-            <div className="card">
-              <div className="content">
-                <div className="font-bold text-3xl">FRIDAY - SUNDAY</div>
-                <div className="flex justify-around">
-                  <div>
-                    <div className="title">9 HOLES</div>
-                    <div className="price text-green-800">$10</div>
-                  </div>
-                  <div>
-                    <div className="title">9 HOLES + CART</div>
-                    <div className="price text-green-800">$28</div>
-                  </div>
-                </div>
-                <div className="text-md">
-                  Enjoy a quick and affordable golfing experience on-foot, or
-                  upgrade your golfing adventure to include a cart full of
-                  modern comforts.
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <AllCards />
+      
+
+
+
+      <div className="grid grid-cols-1  md:grid-cols-12 gap-6  pt-6 px-8">
+       
+         
+          
       </div>
 
+      <div className=" border-2 md:grid-cols-12 gap-6  pt-6 px-8 md:col-span-4 md:order-last mb-2 border-[#7da33f]" style={{}}>
+            <h1 className="text-[#7da33f] font-bold text-2xl text-center">
+              9 HOLE EXECUTIVE COURSE
+            </h1>
+            <div className="px-2">
+              <p className="text-left  my-3">
+              Enjoy a quick game of golf on-foot, or upgrade your golfing adventure to include a cart full of modern comforts to get you around.
+              </p>
+
+              <ul className="list-disc list-inside ">
+                <li className="text-xl uppercase font-semibold">
+                  Monday - Thursday
+                  <br />
+                  <span className="ml-7">7 Am - Sunset</span>
+                  <ul className="list-disc list-inside pl-4">
+                    <li className="lowercase font-thin text-lg">
+                      course - $10/<span className="text-sm">person</span>
+                    </li>
+                    <li className="lowercase font-thin text-lg">
+                      golf cart - $18/<span className="text-sm">person</span>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <ul className="list-disc list-inside mt-4">
+                <li className="text-xl uppercase font-semibold">
+                  Friday - Sunday
+                  <br />
+                  <span className="ml-7">7 Am - Sunset</span>
+                  <ul className="list-disc list-inside pl-4">
+                    <li className="lowercase font-thin text-lg">
+                      course - $10/<span className="text-sm">person</span>
+                    </li>
+                    <li className="lowercase font-thin text-lg">
+                      golf cart - $18/<span className="text-sm">person</span>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+
+              <button className="uppercase outline  outline-1 px-2 text-[#7da33f] w-full text-xl my-3 tracking-wider outline-[#7da33f] ">
+                Book Your Tee time
+              </button>
+            </div>
+
+         
+
+             
+          </div>
+
+          <img
+            src={DrivingRange}
+            alt="Golf Course"
+            className="object-fill col-span-8"
+            style={{ width: "100%", height: "auto" }}
+          />
+
+<img
+            src={Golfer}
+            alt="Golf Course"
+            className="object-fill col-span-8"
+            style={{ width: "100%", height: "auto" }}
+          />
+
+      <div className="text-center my-4 p-1">
+        
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 border-4 gap-4 mb-8 border-red-800   lg:max-w-[90vw] mx-4 lg:mx-auto ">
+        <div className="col-span-2 lg:col-span-1 p-4">
+          <div className="flex flex-col items-center"></div>
+          <p className="tracking-wide text-xl font-primary font-semibold my-3 text-justify">
+            <h1 className="text-maroon font-bold text-4xl font-DinCondensed text-center my-4">
+            DRIVING RANGE
+            </h1>
+            <div className="px-2 text">
+              <p className="text-center  my-4">
+              Want to perfect your swing? Choose your bucket to practice anytime of day even after sunset with our lighted driving range.
+              </p>
+
+              <ul className="list-disc list-inside ">
+                <li className="text-xl uppercase font-semibold">
+                  Monday - Sunday
+                  <br />
+                  <span className="ml-7">7 Am - 9 Pm</span>
+                  <ul className="list-disc list-inside pl-4">
+                    <li className="lowercase font-thin text-lg">
+                      small bucket - $10/<span className="text-sm">person</span>
+                    </li>
+                    <li className="lowercase font-thin text-lg">
+                      medium bucket - $12/<span className="text-sm">person</span>
+                    </li>
+                    <li className="lowercase font-thin text-lg">
+                      large bucket - $15/<span className="text-sm">person</span>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </p>
+        </div>
+
+        <div className="col-span-2 lg:col-span-1 p-4">
+         
+        </div>
+        <button className="uppercase outline  outline-1 px-2 text-[#791214] w-full text-xl my-3 tracking-wider outline-[#791214] ">
+                PLAY MORE SAVE MORE WITH A MEMBERSHIP!
+              </button>
+      </div>
+
+      {/* <LeaveAMessage /> */}
       <Footer />
-    </div>
+    </>
   );
 };
 
